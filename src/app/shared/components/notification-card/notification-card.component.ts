0d6e443f9afddb2571d41f612c3b8f73b73b1e13
Quentin <rousseltin@gmail.com>
2023-08-18 02:24:59 +0200

    Animation de la notification d'erreur

--- src/app/shared/components/notification-card/notification-card.component.ts
+++ src/app/shared/components/notification-card/notification-card.component.ts
@@ -1,10 +1,43 @@
 import { Component, Input } from '@angular/core';
 import { NotificationService } from '../../services/notification.service';
+import {
+  trigger,
+  transition,
+  useAnimation,
+  animate,
+  style,
+} from '@angular/animations';
+import { slideInAnimation } from '../../animations/slide-in.animation';
 
 @Component({
   selector: 'app-notification-card',
   templateUrl: './notification-card.component.html',
   styleUrls: ['./notification-card.component.scss'],
+  animations: [
+    trigger('notif', [
+      transition('void => *', [
+        style({
+          transform: 'translatex(100%)',
+          opacity: 0,
+        }),
+        animate(
+          '500ms ease-out',
+          style({
+            transform: 'translatex(0)',
+            opacity: 1,
+          })
+        ),
+        animate('2000ms'),
+        animate(
+          '700ms ease-out',
+          style({
+            transform: 'translatex(-150%)',
+            opacity: 0,
+          })
+        ),
+      ]),
+    ]),
+  ],
 })
 export class NotificationCardComponent {
   constructor(public notificationService: NotificationService) {}
--- src/app/shared/components/notification/notification.component.ts
+++ src/app/shared/components/notification/notification.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NotificationService } from '../../services/notification.service';
+import { trigger, transition, style, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-notification',
